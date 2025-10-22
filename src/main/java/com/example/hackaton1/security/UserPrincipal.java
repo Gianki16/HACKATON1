4e50e33f1137c3b5b952834d8cef230c318351c7
Giancarlo Humberto Ferreyra Uribe <giancarlohumberto@gmail.com>
2025-10-21 19:04:18 -0500

    Security + .env (EXAMPLE) + H2 POM + ASYNC
    APP Properties.

--- src/main/java/com/example/hackaton1/security/UserPrincipal.java
+++ src/main/java/com/example/hackaton1/security/UserPrincipal.java
@@ -1,4 +1,54 @@
 package com.example.hackaton1.security;
 
-public class UserPrincipal {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private String role;
+    private String branch;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
